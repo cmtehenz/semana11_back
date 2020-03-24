cad3df85f127a93602808ce88c8847e52e2ec557
Gustavo Costa <cmtehenz@gmail.com>
2020-03-24 17:46:58 -0300

    criado usuario

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const routes = express.Router();
+
+const ongController = require("./controllers/OngController");
+const incidentController = require("./controllers/IncidentController");
+const profileController = require("./controllers/ProfileController");
+const sessionController = require("./controllers/SessionController");
+
+routes.post("/sessions", sessionController.create);
+
+routes.get("/ongs", ongController.index);
+routes.post("/ongs", ongController.create);
+
+routes.post("/incidents", incidentController.create);
+routes.get("/incidents", incidentController.index);
+routes.delete("/incidents/:id", incidentController.delete);
+
+routes.get("/profile", profileController.index);
+
+module.exports = routes;
